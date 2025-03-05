497b90500dc8c3a7f476f9a558e419ade618e824
Techkkq <93881136+Tqqk@users.noreply.github.com>
2025-03-05 21:18:37 +0100

    set up Tailwind CSS with initial configuration and example HTML

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // If you're using an HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
