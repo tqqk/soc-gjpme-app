4dbba524626c652a7078cff0f3e77d69a42abc85
Techkkq <93881136+Tqqk@users.noreply.github.com>
2025-03-30 20:17:57 +0200

    frontend styling for quiz page

--- frontend/script/quiz.js
+++ frontend/script/quiz.js
@@ -34,32 +34,49 @@ function displayCurrentQuestion() {
 
   // vytvoření div pro otázku
   const questionDiv = document.createElement("div");
-  questionDiv.classList.add("bg-white", "p-6");
+  questionDiv.classList.add(
+    "p-6", 
+    "rounded", 
+    "bg-white", 
+    "rounded-lg", 
+    "shadow-md",
+  );
 
   // text otázky
   const questionText = document.createElement("p");
-  questionText.classList.add("text-xl", "font-medium", "mb-4");
+  questionText.classList.add(
+    "text-xl", 
+    "font-medium", 
+    "mb-4",  
+ 
+  );
   questionText.textContent = question.question;
   questionDiv.appendChild(questionText);
 
   // vytvoření div pro možnosti odpovědí
   const optionsContainer = document.createElement("div");
-  optionsContainer.classList.add("space-y-2");
+  optionsContainer.classList.add(
+    "flex", 
+    "flex-col", 
+    "gap-3"
+  );
 
   // vytvoření tlačítka pro každou odpověď  
   question.options.forEach((option) => {
     const button = document.createElement("button");
     button.classList.add(
       "w-full",
-      "py-2",
+      "py-3",
       "px-4",
-      "bg-indigo-600",
-      "text-white",
-      "rounded-lg",
-      "hover:bg-indigo-700",
+      "border-2",
+      "border-black",
+      "text-black",
+      "rounded",
+      "hover:bg-black",
+      "hover:text-white",
     );
     button.textContent = option;
-    button.onclick = () => handleAnswer(question.id, option);
+    button.onclick = (event) => handleAnswer(question.id, option, event.target);
     optionsContainer.appendChild(button);
   });
 
@@ -68,11 +85,23 @@ function displayCurrentQuestion() {
 }
 
 // 
-async function handleAnswer(questionId, answer) {
+async function handleAnswer(questionId, answer, clickedButton) {
   try {
 
     // vypnutí tlačítek pro zvolení odpovědi
     Array.from(document.querySelectorAll("button")).forEach(btn => btn.disabled = true);
+
+    // přidání stylu pro zvolenou odpověď
+    if (clickedButton) {
+      clickedButton.classList.remove(
+        "hover:bg-black", 
+        "hover:text-white"
+      );
+      clickedButton.classList.add(
+        "bg-black", 
+        "text-white"
+      );
+    }
 
     const response = await fetch("http://localhost:3000/api/submit", {
       method: "POST",
@@ -83,11 +112,23 @@ async function handleAnswer(questionId, answer) {
 
     if (result.correct) {
       score++;
-      resultContainer.textContent = "Správná odpověď";
-      resultContainer.className = "text-green-500 text-center text-xl mb-4";
+      const correctAnswerMessage = document.createElement("p");
+      correctAnswerMessage.textContent = "Správná odpověď";
+      correctAnswerMessage.classList.add(
+        "text-green-900", 
+        "text-center", 
+        "text-xl"
+      );
+      resultContainer.appendChild(correctAnswerMessage);
     } else {
-        resultContainer.textContent = "Špatná odpověď";
-        resultContainer.className = "text-red-500 text-center text-xl mb-4";
+      const correctAnswerMessage = document.createElement("p");
+      correctAnswerMessage.textContent = "Špatná odpověď";
+      correctAnswerMessage.classList.add(
+        "text-red-900", 
+        "text-center", 
+        "text-xl"
+      );
+      resultContainer.appendChild(correctAnswerMessage);
     }
   } catch (error) {
     resultContainer.textContent = "Chyba při odesílání odpovědi";
@@ -100,13 +141,16 @@ function showNextButton() {
   const nextButton = document.createElement("button");
   nextButton.textContent = "Další otázka";
   nextButton.classList.add(
-    "mt-4",
     "py-2",
     "px-4",
     "mx-4",
-    "bg-blue-600",
-    "text-black",
-    "rounded-lg",
+    "border-2",
+    "bg-black",
+    "border-black",
+    "text-white",
+    "hover:bg-opacity-5",
+    "hover:text-black",
+    "rounded",
     "hover:bg-blue-700",
   );
   nextButton.onclick = () => {
@@ -116,16 +160,27 @@ function showNextButton() {
   resultContainer.appendChild(nextButton);
 }
 
+
 // finální skóre
 function showFinalScore() {
   questionContainer.innerHTML = "";
   resultContainer.innerHTML = "";
 
   const scoreDiv = document.createElement("div");
-  scoreDiv.classList.add("bg-white", "p-6", "rounded-lg", "shadow-md", "text-center");
+  scoreDiv.classList.add(
+    "bg-white", 
+    "p-6", 
+    "rounded-lg", 
+    "shadow-md", 
+    "text-center"
+  );
 
   const scoreText = document.createElement("p");
-  scoreText.classList.add("text-2xl", "font-semibold", "mb-4");
+  scoreText.classList.add(
+    "text-2xl", 
+    "font-semibold", 
+    "mb-4"
+  );
   scoreText.textContent = `Skóre: ${score} z ${questions.length}`;
 
   const restartButton = document.createElement("button");
@@ -133,10 +188,13 @@ function showFinalScore() {
   restartButton.classList.add(
     "py-2",
     "px-4",
-    "bg-green-600",
-    "text-black",
+    "bg-black",
+    "text-white",
+    "border-2",
+    "border-black",
     "rounded-lg",
-    "hover:bg-green-700",
+    "hover:bg-opacity-5",
+    "hover:text-black",
   );
   restartButton.onclick = restartQuiz;
 
